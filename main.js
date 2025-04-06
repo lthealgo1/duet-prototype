5974c501d24f81be288c6f5d26b5cbbe26a6d02a
Linus Barnett <linusbarnett@icloud.com>
2025-04-06 23:41:28 +0100

    Added GET request upon every webhook event

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
+// import dotenv from "dotenv";
+// import fs from "fs";
+const fs = require('fs')
+const dotenv = require('dotenv');
+const { Octokit } = require("octokit");
+dotenv.config();
+
+const appId = process.env.APP_ID;
+const webhookSecret = process.env.WEBHOOK_SECRET;
+const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+const oauth = process.env.OAUTH; // this is all that is needed - PAT - which admin can give read-access to. (faily certain)
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+
+// const app = new App({
+//   appId: appId,
+//   privateKey: privateKey,
+//   webhooks: {
+//     secret: webhookSecret
+//   },
+// });
+
 const express = require('express');
 const app = express();
+const octokit = new Octokit ({
+  auth: oauth
+})
 
-// import api_get_request function, will use it for every URL returned by Webhook when an event occurs.
 // Handler for AWS Lambda. 
 
 // This defines a POST route at the `/webhook` path. 
@@ -15,10 +39,13 @@ app.post('/webhook', express.json({type: 'application/json'}), (request, respons
 
   if (githubEvent === 'issues') {
     const data = request.body;
-    console.log(data)
-    const issue_url = data.issue.url;
+    //console.log(data)
     const login = data.issue.user.login;
     const action = data.action;
+
+    const owner = data.repository.owner.login;
+    const repo = data.repository.name;
+    const path = data.repository.full_name;
     if (action === 'opened') {
       console.log(`An issue was opened with this title: ${data.issue.title} by user: ${login}.`);
     } else if (action === 'closed') {
@@ -26,10 +53,29 @@ app.post('/webhook', express.json({type: 'application/json'}), (request, respons
     } else {
       console.log(`Unhandled action for the issue event: ${action} by ${login}.`);
     }
-    console.log(issue_url);// will be needed for -> repo_code = api_get_request(pr_url)... this function is not finished yet
-    
+    octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: owner,
+      repo: repo,
+      path: '',
+      headers: {
+        "Accept": "application/vnd.github.v3.raw+json",
+        'X-GitHub-Api-Version': '2022-11-28'
+    }})
+    .then(response => {
+
+      // const {content, download_url} = response.data;
+    //console.log(response.data);
+    const data = response.data;
+    const fileNames = data.map(file => file.name); 
+    console.log(fileNames)
+    const fileURLs = data.map(file => file.download_url)
+    console.log(fileURLs)
+    // download using urls comes next
+    })
+
+    // console.log(issue_url);
+    // will be needed for -> repo_code = api_get_request(pr_url)... this function is not finished yet
     // api_get_request function in separate file will also need a Personal Access token and a Github actions token - will be located in .gitignore
-    
     // once repo_code is returned, db_api called to store the code in a table / sent straight to script with Anthropic plugin.
   } 
   
@@ -43,7 +89,30 @@ app.post('/webhook', express.json({type: 'application/json'}), (request, respons
     const author = data.sender.login;
     const push_url = data.commits.url;
     console.log(`A push was made by author ${author} to repository: ${repname}`);
-    console.log(push_url);// will be needed for -> repo_code = api_get_request(push_url)... this function is not finished yet
+
+    const owner = data.repository.owner.login;
+    const repo = data.repository.name;
+    const path = data.repository.full_name;
+    octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: owner,
+      repo: repo,
+      path: '',
+      headers: {
+        "Accept": "application/vnd.github.v3.raw+json",
+        'X-GitHub-Api-Version': '2022-11-28'
+    }})
+    .then(response => {
+
+      // const {content, download_url} = response.data;
+    //console.log(response.data);
+    const data = response.data;
+    const fileNames = data.map(file => file.name); 
+    console.log(fileNames)
+    const fileURLs = data.map(file => file.download_url)
+    console.log(fileURLs)
+    // download using urls comes next
+    })
+    // will be needed for -> repo_code = api_get_request(push_url)... this function is not finished yet
 
     // api_get_request function in separate file will also need a Personal Access token and a Github actions token - will be located in .gitignore
 
@@ -64,8 +133,33 @@ app.post('/webhook', express.json({type: 'application/json'}), (request, respons
     else if (action === 'closed') {
       console.log(`A pull request, with change(s) ${change} was closed by user: ${login}.\nCreated at ${created_at}, updated at ${updated_at}`);
     }
-    const pr_url = pull_request.url;
-    console.log(pr_url);// will be needed for -> repo_code = api_get_request(pr_url)... this function is not finished yet
+    // const pr_url = pull_request.url;
+    // console.log(pr_url);
+    const owner = data.repository.owner.login;
+    const repo = data.repository.name;
+    const path = data.repository.full_name;
+    octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: owner,
+      repo: repo,
+      path: '',
+      headers: {
+        "Accept": "application/vnd.github.v3.raw+json",
+        'X-GitHub-Api-Version': '2022-11-28'
+    }})
+    .then(response => {
+
+      // const {content, download_url} = response.data;
+    //console.log(response.data);
+    const data = response.data;
+    const fileNames = data.map(file => file.name); 
+    console.log(fileNames)
+    const fileURLs = data.map(file => file.download_url)
+    console.log(fileURLs)
+    // download using urls comes next
+    })
+
+
+    // will be needed for -> repo_code = api_get_request(pr_url)... this function is not finished yet
     
     // api_get_request function in separate file will also need a Personal Access token and a Github actions token - will be located in .gitignore
     
@@ -76,14 +170,37 @@ app.post('/webhook', express.json({type: 'application/json'}), (request, respons
   else if (githubEvent === 'fork') {
     const data = request.body;
     const author = data.sender.login; 
-    const repo = data.forkee.repository;
+    const repo_forked = data.forkee.repository;
     const fork_url = data.forkee.url;
 
-    console.log(`A fork request was made by ${author} from repo ${repo}.`)
-    console.log(fork_url)// will be needed for -> repo_code = api_get_request(pr_url)... this function is not finished yet
-    
+    console.log(`A fork request was made by ${author} from repo ${repo_forked}.`)
+
+    const owner = data.repository.owner.login;
+    const repo = data.repository.name;
+    const path = data.repository.full_name;
+    octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: owner,
+      repo: repo,
+      path: '',
+      headers: {
+        "Accept": "application/vnd.github.v3.raw+json",
+        'X-GitHub-Api-Version': '2022-11-28'
+    }})
+    .then(response => {
+
+      // const {content, download_url} = response.data;
+    //console.log(response.data);
+    const data = response.data;
+    const fileNames = data.map(file => file.name); 
+    console.log(fileNames)
+    const fileURLs = data.map(file => file.download_url)
+    console.log(fileURLs)
+    // download using urls comes next
+    })
+
+
+    // will be needed for -> repo_code = api_get_request(pr_url)... this function is not finished yet
     // api_get_request function in separate file will also need a Personal Access token and a Github actions token - will be located in .gitignore
-    
     // once repo_code is returned, db_api called to store the code in a table / sent straight to script with Anthropic plugin.
   }
 
